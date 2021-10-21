9d9af1d54f4f17f35171fa6ec8fdea6b597119ac
Hein Coetzee <heinctz@outlook.com>
2021-10-21 12:37:29 +0200

    Design Tamagochi home form

--- frmHome.Designer.cs
+++ frmHome.Designer.cs
@@ -29,19 +29,165 @@ namespace INF164HWAss1
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.btnGame = new System.Windows.Forms.Button();
+            this.btnKitchen = new System.Windows.Forms.Button();
+            this.btnSleep = new System.Windows.Forms.Button();
+            this.lblGold = new System.Windows.Forms.Label();
+            this.gbxTop = new System.Windows.Forms.GroupBox();
+            this.lblGameLevel = new System.Windows.Forms.Label();
+            this.lblHungerLevel = new System.Windows.Forms.Label();
+            this.lblSleepLevel = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.gbxBottom = new System.Windows.Forms.GroupBox();
+            this.imlTamagochiState = new System.Windows.Forms.ImageList(this.components);
+            this.gbxTop.SuspendLayout();
+            this.gbxBottom.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnGame
+            // 
+            this.btnGame.Location = new System.Drawing.Point(115, 19);
+            this.btnGame.Name = "btnGame";
+            this.btnGame.Size = new System.Drawing.Size(63, 40);
+            this.btnGame.TabIndex = 0;
+            this.btnGame.Text = "Game";
+            this.btnGame.UseVisualStyleBackColor = true;
+            // 
+            // btnKitchen
+            // 
+            this.btnKitchen.Location = new System.Drawing.Point(212, 19);
+            this.btnKitchen.Name = "btnKitchen";
+            this.btnKitchen.Size = new System.Drawing.Size(71, 40);
+            this.btnKitchen.TabIndex = 1;
+            this.btnKitchen.Text = "Kitchen";
+            this.btnKitchen.UseVisualStyleBackColor = true;
+            // 
+            // btnSleep
+            // 
+            this.btnSleep.Location = new System.Drawing.Point(319, 19);
+            this.btnSleep.Name = "btnSleep";
+            this.btnSleep.Size = new System.Drawing.Size(63, 40);
+            this.btnSleep.TabIndex = 2;
+            this.btnSleep.Text = "Sleep";
+            this.btnSleep.UseVisualStyleBackColor = true;
+            // 
+            // lblGold
+            // 
+            this.lblGold.AutoSize = true;
+            this.lblGold.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblGold.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGold.Location = new System.Drawing.Point(12, 40);
+            this.lblGold.Name = "lblGold";
+            this.lblGold.Size = new System.Drawing.Size(56, 15);
+            this.lblGold.TabIndex = 3;
+            this.lblGold.Text = "Gold: 0";
+            // 
+            // gbxTop
+            // 
+            this.gbxTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gbxTop.Controls.Add(this.btnExit);
+            this.gbxTop.Controls.Add(this.lblSleepLevel);
+            this.gbxTop.Controls.Add(this.lblHungerLevel);
+            this.gbxTop.Controls.Add(this.lblGameLevel);
+            this.gbxTop.Controls.Add(this.lblGold);
+            this.gbxTop.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxTop.Location = new System.Drawing.Point(0, -5);
+            this.gbxTop.Name = "gbxTop";
+            this.gbxTop.Size = new System.Drawing.Size(529, 85);
+            this.gbxTop.TabIndex = 5;
+            this.gbxTop.TabStop = false;
+            // 
+            // lblGameLevel
+            // 
+            this.lblGameLevel.AutoSize = true;
+            this.lblGameLevel.BackColor = System.Drawing.Color.Lime;
+            this.lblGameLevel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameLevel.Location = new System.Drawing.Point(112, 19);
+            this.lblGameLevel.Name = "lblGameLevel";
+            this.lblGameLevel.Size = new System.Drawing.Size(70, 15);
+            this.lblGameLevel.TabIndex = 4;
+            this.lblGameLevel.Text = "Game: 100";
+            // 
+            // lblHungerLevel
+            // 
+            this.lblHungerLevel.AutoSize = true;
+            this.lblHungerLevel.BackColor = System.Drawing.Color.Lime;
+            this.lblHungerLevel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHungerLevel.Location = new System.Drawing.Point(202, 19);
+            this.lblHungerLevel.Name = "lblHungerLevel";
+            this.lblHungerLevel.Size = new System.Drawing.Size(84, 15);
+            this.lblHungerLevel.TabIndex = 5;
+            this.lblHungerLevel.Text = "Hunger: 100";
+            // 
+            // lblSleepLevel
+            // 
+            this.lblSleepLevel.AutoSize = true;
+            this.lblSleepLevel.BackColor = System.Drawing.Color.Lime;
+            this.lblSleepLevel.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSleepLevel.Location = new System.Drawing.Point(305, 19);
+            this.lblSleepLevel.Name = "lblSleepLevel";
+            this.lblSleepLevel.Size = new System.Drawing.Size(77, 15);
+            this.lblSleepLevel.TabIndex = 6;
+            this.lblSleepLevel.Text = "Sleep: 100";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Tomato;
+            this.btnExit.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(444, 31);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(51, 33);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // gbxBottom
+            // 
+            this.gbxBottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gbxBottom.Controls.Add(this.btnGame);
+            this.gbxBottom.Controls.Add(this.btnKitchen);
+            this.gbxBottom.Controls.Add(this.btnSleep);
+            this.gbxBottom.Location = new System.Drawing.Point(0, 394);
+            this.gbxBottom.Name = "gbxBottom";
+            this.gbxBottom.Size = new System.Drawing.Size(529, 81);
+            this.gbxBottom.TabIndex = 4;
+            this.gbxBottom.TabStop = false;
+            // 
+            // imlTamagochiState
+            // 
+            this.imlTamagochiState.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imlTamagochiState.ImageSize = new System.Drawing.Size(30, 30);
+            this.imlTamagochiState.TransparentColor = System.Drawing.Color.Transparent;
             // 
             // frmHome
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 450);
+            this.ClientSize = new System.Drawing.Size(529, 474);
+            this.Controls.Add(this.gbxTop);
+            this.Controls.Add(this.gbxBottom);
             this.Name = "frmHome";
             this.Text = "frmHome";
+            this.gbxTop.ResumeLayout(false);
+            this.gbxTop.PerformLayout();
+            this.gbxBottom.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGame;
+        private System.Windows.Forms.Button btnKitchen;
+        private System.Windows.Forms.Button btnSleep;
+        private System.Windows.Forms.Label lblGold;
+        private System.Windows.Forms.GroupBox gbxTop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblSleepLevel;
+        private System.Windows.Forms.Label lblHungerLevel;
+        private System.Windows.Forms.Label lblGameLevel;
+        private System.Windows.Forms.GroupBox gbxBottom;
+        private System.Windows.Forms.ImageList imlTamagochiState;
     }
 }
