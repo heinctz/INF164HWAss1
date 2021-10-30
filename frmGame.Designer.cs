6515b1f4cdec8e34502aaaf8b7435ada722e650c
Rainier van der Walt <rainiervdw@gmail.com>
2021-10-30 10:58:29 +0200

    Design game form

--- frmGame.Designer.cs
+++ frmGame.Designer.cs
@@ -29,20 +29,310 @@ namespace INF164HWAss1
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnA1 = new System.Windows.Forms.Button();
+            this.btnA2 = new System.Windows.Forms.Button();
+            this.btnA3 = new System.Windows.Forms.Button();
+            this.btnB1 = new System.Windows.Forms.Button();
+            this.btnB2 = new System.Windows.Forms.Button();
+            this.btnB3 = new System.Windows.Forms.Button();
+            this.btnC1 = new System.Windows.Forms.Button();
+            this.btnC2 = new System.Windows.Forms.Button();
+            this.btnC3 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblTamagochi = new System.Windows.Forms.Label();
+            this.lblDraw = new System.Windows.Forms.Label();
+            this.lblComputer = new System.Windows.Forms.Label();
+            this.lblWincount = new System.Windows.Forms.Label();
+            this.lblDrawcount = new System.Windows.Forms.Label();
+            this.lblLosecount = new System.Windows.Forms.Label();
+            this.lblPlaying = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnA1
+            // 
+            this.btnA1.Location = new System.Drawing.Point(67, 67);
+            this.btnA1.Name = "btnA1";
+            this.btnA1.Size = new System.Drawing.Size(75, 75);
+            this.btnA1.TabIndex = 0;
+            this.btnA1.UseVisualStyleBackColor = true;
+            // 
+            // btnA2
+            // 
+            this.btnA2.Location = new System.Drawing.Point(160, 67);
+            this.btnA2.Name = "btnA2";
+            this.btnA2.Size = new System.Drawing.Size(75, 75);
+            this.btnA2.TabIndex = 1;
+            this.btnA2.UseVisualStyleBackColor = true;
+            // 
+            // btnA3
+            // 
+            this.btnA3.Location = new System.Drawing.Point(252, 67);
+            this.btnA3.Name = "btnA3";
+            this.btnA3.Size = new System.Drawing.Size(75, 75);
+            this.btnA3.TabIndex = 2;
+            this.btnA3.UseVisualStyleBackColor = true;
+            // 
+            // btnB1
+            // 
+            this.btnB1.Location = new System.Drawing.Point(67, 159);
+            this.btnB1.Name = "btnB1";
+            this.btnB1.Size = new System.Drawing.Size(75, 75);
+            this.btnB1.TabIndex = 3;
+            this.btnB1.UseVisualStyleBackColor = true;
+            // 
+            // btnB2
+            // 
+            this.btnB2.Location = new System.Drawing.Point(160, 159);
+            this.btnB2.Name = "btnB2";
+            this.btnB2.Size = new System.Drawing.Size(75, 75);
+            this.btnB2.TabIndex = 4;
+            this.btnB2.UseVisualStyleBackColor = true;
+            // 
+            // btnB3
+            // 
+            this.btnB3.Location = new System.Drawing.Point(252, 159);
+            this.btnB3.Name = "btnB3";
+            this.btnB3.Size = new System.Drawing.Size(75, 75);
+            this.btnB3.TabIndex = 5;
+            this.btnB3.UseVisualStyleBackColor = true;
+            // 
+            // btnC1
+            // 
+            this.btnC1.Location = new System.Drawing.Point(68, 251);
+            this.btnC1.Name = "btnC1";
+            this.btnC1.Size = new System.Drawing.Size(75, 75);
+            this.btnC1.TabIndex = 6;
+            this.btnC1.UseVisualStyleBackColor = true;
+            // 
+            // btnC2
+            // 
+            this.btnC2.Location = new System.Drawing.Point(160, 251);
+            this.btnC2.Name = "btnC2";
+            this.btnC2.Size = new System.Drawing.Size(75, 75);
+            this.btnC2.TabIndex = 7;
+            this.btnC2.UseVisualStyleBackColor = true;
+            // 
+            // btnC3
+            // 
+            this.btnC3.Location = new System.Drawing.Point(252, 251);
+            this.btnC3.Name = "btnC3";
+            this.btnC3.Size = new System.Drawing.Size(75, 75);
+            this.btnC3.TabIndex = 8;
+            this.btnC3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(149, 67);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 257);
+            this.panel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Location = new System.Drawing.Point(241, 67);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 257);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DimGray;
+            this.panel3.Location = new System.Drawing.Point(68, 148);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(260, 5);
+            this.panel3.TabIndex = 11;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DimGray;
+            this.panel4.Location = new System.Drawing.Point(68, 240);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(260, 5);
+            this.panel4.TabIndex = 12;
+            // 
+            // lblTamagochi
+            // 
+            this.lblTamagochi.AutoSize = true;
+            this.lblTamagochi.Location = new System.Drawing.Point(65, 351);
+            this.lblTamagochi.Name = "lblTamagochi";
+            this.lblTamagochi.Size = new System.Drawing.Size(66, 13);
+            this.lblTamagochi.TabIndex = 13;
+            this.lblTamagochi.Text = "Tamagochi: ";
+            // 
+            // lblDraw
+            // 
+            this.lblDraw.AutoSize = true;
+            this.lblDraw.Location = new System.Drawing.Point(175, 351);
+            this.lblDraw.Name = "lblDraw";
+            this.lblDraw.Size = new System.Drawing.Size(38, 13);
+            this.lblDraw.TabIndex = 14;
+            this.lblDraw.Text = "Draw: ";
+            // 
+            // lblComputer
+            // 
+            this.lblComputer.AutoSize = true;
+            this.lblComputer.Location = new System.Drawing.Point(249, 351);
+            this.lblComputer.Name = "lblComputer";
+            this.lblComputer.Size = new System.Drawing.Size(58, 13);
+            this.lblComputer.TabIndex = 15;
+            this.lblComputer.Text = "Computer: ";
+            // 
+            // lblWincount
+            // 
+            this.lblWincount.AutoSize = true;
+            this.lblWincount.Location = new System.Drawing.Point(130, 351);
+            this.lblWincount.Name = "lblWincount";
+            this.lblWincount.Size = new System.Drawing.Size(13, 13);
+            this.lblWincount.TabIndex = 16;
+            this.lblWincount.Text = "0";
+            // 
+            // lblDrawcount
+            // 
+            this.lblDrawcount.AutoSize = true;
+            this.lblDrawcount.Location = new System.Drawing.Point(210, 351);
+            this.lblDrawcount.Name = "lblDrawcount";
+            this.lblDrawcount.Size = new System.Drawing.Size(13, 13);
+            this.lblDrawcount.TabIndex = 17;
+            this.lblDrawcount.Text = "0";
+            // 
+            // lblLosecount
+            // 
+            this.lblLosecount.AutoSize = true;
+            this.lblLosecount.Location = new System.Drawing.Point(304, 351);
+            this.lblLosecount.Name = "lblLosecount";
+            this.lblLosecount.Size = new System.Drawing.Size(13, 13);
+            this.lblLosecount.TabIndex = 18;
+            this.lblLosecount.Text = "0";
+            // 
+            // lblPlaying
+            // 
+            this.lblPlaying.AutoSize = true;
+            this.lblPlaying.Location = new System.Drawing.Point(157, 33);
+            this.lblPlaying.Name = "lblPlaying";
+            this.lblPlaying.Size = new System.Drawing.Size(69, 13);
+            this.lblPlaying.TabIndex = 19;
+            this.lblPlaying.Text = "Playing time: ";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(222, 33);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(13, 13);
+            this.lblMin.TabIndex = 20;
+            this.lblMin.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = ":";
+            // 
+            // lblSec
+            // 
+            this.lblSec.AutoSize = true;
+            this.lblSec.Location = new System.Drawing.Point(249, 33);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(13, 13);
+            this.lblSec.TabIndex = 22;
+            this.lblSec.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(400, 24);
+            this.menuStrip1.TabIndex = 23;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.newGameToolStripMenuItem.Text = "New game";
             // 
             // frmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 450);
+            this.ClientSize = new System.Drawing.Size(400, 399);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblPlaying);
+            this.Controls.Add(this.lblLosecount);
+            this.Controls.Add(this.lblDrawcount);
+            this.Controls.Add(this.lblWincount);
+            this.Controls.Add(this.lblComputer);
+            this.Controls.Add(this.lblDraw);
+            this.Controls.Add(this.lblTamagochi);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnC3);
+            this.Controls.Add(this.btnC2);
+            this.Controls.Add(this.btnC1);
+            this.Controls.Add(this.btnB3);
+            this.Controls.Add(this.btnB2);
+            this.Controls.Add(this.btnB1);
+            this.Controls.Add(this.btnA3);
+            this.Controls.Add(this.btnA2);
+            this.Controls.Add(this.btnA1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmGame";
             this.Text = "frmGame";
             this.Shown += new System.EventHandler(this.frmGame_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnA1;
+        private System.Windows.Forms.Button btnA2;
+        private System.Windows.Forms.Button btnA3;
+        private System.Windows.Forms.Button btnB1;
+        private System.Windows.Forms.Button btnB2;
+        private System.Windows.Forms.Button btnB3;
+        private System.Windows.Forms.Button btnC1;
+        private System.Windows.Forms.Button btnC2;
+        private System.Windows.Forms.Button btnC3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblTamagochi;
+        private System.Windows.Forms.Label lblDraw;
+        private System.Windows.Forms.Label lblComputer;
+        private System.Windows.Forms.Label lblWincount;
+        private System.Windows.Forms.Label lblDrawcount;
+        private System.Windows.Forms.Label lblLosecount;
+        private System.Windows.Forms.Label lblPlaying;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
     }
 }
